0819177bba405340f086a8f5bc677a76507ab3cb
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-08-11 11:29:30 +0100

    Successful login brings you to table-data

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 
 const login = (username, password) => {
   if (username === 'admin' && password === 'admin') {
-    console.log('hello world');
+    window.location.replace("/data-table.html")
   } else {
     alert('Incorrect sign-in details');
   }
@@ -12,6 +12,5 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const username = document.getElementById('username1').value;
   const password = document.getElementById('password1').value;
-  console.log(username, password);
   login(username, password);
 });
